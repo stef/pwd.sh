8dcc87cc4da060b441208fd0907aff792bfead0b
stef <stefan.marsiske@gmail.com>
2013-04-03 04:22:18 +0200

    [enh] big surprise: bash, signed, no-gpg-agent, clipboard, bg window, param

--- pwd.sh
+++ pwd.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/ksh
+#!/usr/bin/env bash
 # (c) 2013 s@ctrlc.hu, asciimoo@faszkorbacs.hu, mail@crazypotato.tk
 #
 #  This is free software; you can redistribute it and/or modify
@@ -34,31 +34,31 @@
 # gpg --gen-key --homedir ~/.pwd/gnupg
 
 # how you like your passwords?
-alias pwgen="apg -q -a1 -n 1 -m 14 -M NCL"
+function pwgen { /usr/bin/apg -q -a1 -n 1 -m 14 -M NCL; }
 
 # ssh-askpass path fix for ARCH linux
 if [[ -x "/usr/lib/ssh/ssh-askpass" ]]; then
-    alias pwprompt=/usr/lib/ssh/ssh-askpass
+    function pwprompt { /usr/lib/ssh/ssh-askpass; }
 else
-    alias pwprompt=ssh-askpass
+    function pwprompt { /usr/bin/ssh-askpass; }
 fi
 
 # end of config
 
 function userprompt {
-    echo -e "$USER\n$(xclip -o)" | dmenu -p 'user>'
+    echo -e "$USER\n$(/usr/bin/xclip -o)" | /usr/bin/dmenu -p 'user>'
 }
 
 function xdoget {
     title="$1"
     shift 1
-    echo -n '' | xclip -i
+    echo -n '' | /usr/bin/xclip -i
     sleep 0.2
-    xdotool getactivewindow key $*
+    /usr/bin/xdotool key --window $windowid $*
     retries=0
     while [[ retries -lt 3 ]]; do
         sleep 0.2
-        x=$(xclip -o)
+        x=$(/usr/bin/xclip -o)
         [[ "$x" =~ ^https?:.* ]] && {
             echo $x | cut -d'/' -f1-3
             break
@@ -86,7 +86,7 @@ function reagent {
 
 # use $1 to pass an alternative root-dir for the password store.
 # TODO proper parameter handling someday
-if [[ -n "$1" && "$1" != "a" ]]; then
+if [[ -n "$1" && -f "$1/.cfg" ]]; then
     data="$1"
     shift 1
 else
@@ -98,38 +98,40 @@ source $data/.cfg
 salt=${salt:-anti-rainbow-garbage} # this should be some random string
 
 # if $1 = host and $2 = user get host/user passwords
-[[ "$#" -eq 2 ]] && {
-    { echo -n "$salt"; echo "$1"; } | md5sum | cut -d' ' -f1 | read hosthash
-    { echo -n "$salt"; echo "$2"; } | md5sum | cut -d' ' -f1 | read userhash
+# Todo fix parameter handling
+[[ "$#" -eq 3 ]] && {
+    hosthash="$( { echo -n "$salt"; echo "$1"; } | /usr/bin/md5sum | cut -d' ' -f1 )"
+    userhash="$( { echo -n "$salt"; echo "$2"; } | /usr/bin/md5sum | cut -d' ' -f1 )"
     noagent
-    line=$(pwprompt | gpg --batch --no-tty --quiet -d --passphrase-fd 0 $gpghome -d ~/.pwd/$hosthash/$userhash )
+    line=$(pwprompt | /usr/bin/gpg --batch --no-tty --quiet -d --passphrase-fd 0 $gpghome -d ~/.pwd/$hosthash/$userhash )
     reagent
-    echo -n "${line}" | cut -d"	" -f2 | xclip -i
+    echo -n "${line}" | cut -d"	" -f2 | /usr/bin/xclip -i -selection clipboard
     exit 0
 }
 
 # find out title/url of active window
-title=$(xdotool getactivewindow getwindowname | sed -e 's/^ *//g;s/ *$//g')
+windowid=$(/usr/bin/xdotool getactivewindow)
+title=$(/usr/bin/xdotool getwindowname $windowid | /bin/sed -e 's/^ *//g;s/ *$//g')
 case $title in
-    *Pentadactyl|*Vimperator) title="$(xdoget "$title" Escape y)"; wintype=dactyl; break;;
-    *Iceweasel|*Firefox) title="$(xdoget "$title" Escape ctrl+l ctrl+a ctrl+c)"; wintype=firefox; break;;
-    *Chromium) title="$(xdoget "$title" Escape ctrl+l ctrl+a ctrl+c)"; wintype=chromium; break;;
-    *Uzbl\ browser*) title="$(xdoget "title" Escape y u)"; wintype=uzbl; break;;
-    luakit*) title="$(xdoget "title" shift+o Home ctrl+Right Right ctrl+shift+End ctrl+c Escape)"; wintype=luakit; break;;
+    *Pentadactyl|*Vimperator) title="$(xdoget "$title" Escape y)"; wintype=dactyl;;
+    *Iceweasel|*Firefox) title="$(xdoget "$title" Escape ctrl+l ctrl+a ctrl+c)"; wintype=firefox;;
+    *Chromium) title="$(xdoget "$title" Escape ctrl+l ctrl+a ctrl+c)"; wintype=chromium;;
+    *Uzbl\ browser*) title="$(xdoget "title" Escape y u)"; wintype=uzbl;;
+    luakit*) title="$(xdoget "title" shift+o Home ctrl+Right Right ctrl+shift+End ctrl+c Escape)"; wintype=luakit;;
 esac
 # get hash of title/url
-{ echo -n "$salt"; echo "$title"; } | md5sum | cut -d' ' -f1 | read hash
+hash="$( { echo -n "$salt"; echo "$title"; } | /usr/bin/md5sum | cut -d' ' -f1 )"
 
 # if invoked with $1 = a
 # add a new random password with the current window
 [[ "$1" == "a" ]] && {
     pwd=$(pwgen)
     user=$(userprompt)
-    { echo -n "$salt"; echo "$user"; } | md5sum | cut -d' ' -f1 | read userhash
+    userhash="$( { echo -n "$salt"; echo "$user"; } | /usr/bin/md5sum | cut -d' ' -f1 )"
     mkdir -p ~/.pwd/$hash
     [[ -f ~/.pwd/$hash/$userhash ]] && mv ~/.pwd/$hash/$userhash ~/.pwd/$hash/$userhash.$(date +%s)
-    echo -n "$user	$pwd" | gpg --yes --batch --no-tty --quiet $gpghome --encrypt -r $keyid >~/.pwd/$hash/$userhash
-    echo -n "$pwd" | xclip -i
+    echo -n "$user	$pwd" | /usr/bin/gpg --command-file <(pwprompt) --yes --no-tty --quiet $gpghome --sign --local-user $keyid --encrypt -r $keyid >~/.pwd/$hash/$userhash
+    echo -n "$pwd" | /usr/bin/xclip -i -selection clipboard
     exit 0
 }
 
@@ -139,27 +141,28 @@ esac
 
 noagent
 pass="$(pwprompt)"
-for key in ~/.pwd/$hash/* ; do
-    # todo adapt
-    line=$(echo "$pass" | gpg --batch --no-tty --quiet --passphrase-fd 0 $gpghome -d $key )
-    user="$(echo "${line}" | cut -d"	" -f1)"
-    echo "$user"
-    sleep 0.2
-done | dmenu | read user
+user="$(for key in ~/.pwd/$hash/* ; do
+            fname="${key##*/}"; [[ "$fname" =~ .*\..* ]] && continue
+            # todo adapt
+            line=$(echo "$pass" | /usr/bin/gpg --batch --no-tty --quiet --passphrase-fd 0 $gpghome -d $key )
+            user="$(echo "${line}" | cut -d"	" -f1)"
+            echo "$user"
+            sleep 0.2
+        done | /usr/bin/dmenu)"
 
 [[ -n "$user" ]] && {
-    { echo -n "$salt"; echo "$user"; } | md5sum | cut -d' ' -f1 | read userhash
-    line=$(echo "$pass" | gpg --batch --no-tty --quiet --passphrase-fd 0 $gpghome -d ~/.pwd/$hash/$userhash )
-    echo -n "${line}" | cut -d"	" -f2 | xclip -i
+    userhash="$( { echo -n "$salt"; echo "$user"; } | /usr/bin/md5sum | cut -d' ' -f1)"
+    line=$(echo "$pass" | /usr/bin/gpg --batch --no-tty --quiet --passphrase-fd 0 $gpghome -d ~/.pwd/$hash/$userhash )
+    echo -n "${line}" | cut -d"	" -f2 | /usr/bin/xclip -i -selection clipboard
     [[ -n "$wintype" ]] && {
         case $wintype in
-            dactyl) xdotool getactivewindow key Escape g i ctrl+u ;;
-            luakit) xdotool getactivewindow key Escape g i Tab ctrl+u ;;
-            firefox) xdotool getactivewindow key Tab Tab Tab ;;
-            chromium) xdotool getactivewindow key Tab ;;
+            dactyl) /usr/bin/xdotool key --window $windowid Escape g i ctrl+u ;;
+            luakit) /usr/bin/xdotool key --window $windowid Escape g i Tab ctrl+u ;;
+            firefox) /usr/bin/xdotool key --window $windowid Tab Tab Tab ;;
+            chromium) /usr/bin/xdotool key --window $windowid Tab ;;
         esac
-        xdotool getactivewindow type "$user"
-        xdotool getactivewindow key Tab
+        /usr/bin/xdotool type --window $windowid "$user"
+        /usr/bin/xdotool key --window $windowid Tab
     }
 }
 reagent
